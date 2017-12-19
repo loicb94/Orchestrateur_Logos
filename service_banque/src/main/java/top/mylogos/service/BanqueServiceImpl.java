6db0bda3c5d1a47967f82e305ab3aedc3da7a761
loico <brawandl@yahoo.fr>
2017-12-19 15:56:10 +0100

    Commit du premier service banque vide fonctionnel

--- service_banque/src/main/java/top/mylogos/service/BanqueServiceImpl.java
+++ service_banque/src/main/java/top/mylogos/service/BanqueServiceImpl.java
@@ -3,10 +3,13 @@ package top.mylogos.service;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.springframework.stereotype.Service;
+
 import top.mylogos.entity.DemandePaiement;
 import top.mylogos.entity.ReponsePaiement;
 
-@WebService
+@Service
+@WebService(endpointInterface = "top.mylogos.service.BanqueService")
 public class BanqueServiceImpl implements BanqueService {
 
 	@Override
